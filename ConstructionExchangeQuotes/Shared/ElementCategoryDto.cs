65d5dcd263f2b26a9b2ae689f178ab7492d5afc0
AnteVuletic <antevuletic1995@gmail.com>
2020-06-05 20:55:32 +0200

    CRD for category and element type

--- ConstructionExchangeQuotes/Shared/ElementCategoryDto.cs
+++ ConstructionExchangeQuotes/Shared/ElementCategoryDto.cs
@@ -1,8 +1,13 @@
-﻿namespace ConstructionExchangeQuotes.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConstructionExchangeQuotes.Shared
 {
     public class ElementCategoryDto
     {
         public int? Id { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Minimum category name is 3 characters")]
         public string Name { get; set; }
     }
 }
--- ConstructionExchangeQuotes/Shared/ElementDto.cs
+++ ConstructionExchangeQuotes/Shared/ElementDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ConstructionExchangeQuotes.Shared
 {
@@ -11,10 +12,15 @@ namespace ConstructionExchangeQuotes.Shared
             ElementFields = new List<ElementFieldDto>();
         }
         public int? Id { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Minimum name length is 2")]
         public string Name { get; set; }
+        [Required]
         public double Rate { get; set; }
 
+        [Required]
         public ElementTypeDto ElementType { get; set; }
+        [Required]
         public ElementCategoryDto ElementCategory { get; set; }
 
         public ICollection<ElementFieldDto> ElementFields { get; set; }
